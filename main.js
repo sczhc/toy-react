e8fd5e01e76a5c3a0b766ba73df99c5a08834c15
jason <1415650291@qq.com>
2020-10-12 18:02:58 +0800

    main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 import { createElement, render, Component } from './toy-react';
 
 class MyComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      a: 1
+    }
+  }
   render() {
     return <div>
       <h1>my component</h1>
-      {this.children}
+      <span>{this.state.a.toString()}</span>
+      <button onclick={() => {this.state.a++;this.rerender();}}>add</button>
     </div>
   }
 }
 
-let html = <MyComponent class="my-component">
-  <div class="a">
-    <div>abc</div>
-  </div>
-</MyComponent>;
+let html = <MyComponent class="my-component"></MyComponent>;
 
 render(html, document.getElementById('toy-react'));
